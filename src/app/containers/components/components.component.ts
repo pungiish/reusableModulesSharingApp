52dd32e4b4d89c2b42178b1f40e9a779788f59f1
jan pungarsek <jan.pungarsek@gmail.com>
2019-08-13 11:58:12 +0200

    styling.

--- src/app/containers/components/components.component.ts
+++ src/app/containers/components/components.component.ts
@@ -6,60 +6,54 @@ import { WidgetService } from 'src/app/services/widget-service.service';
 import { User } from 'src/app/models/user-model';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
+
 @Component({
 	selector: 'app-components',
 	templateUrl: './components.component.html',
 	styleUrls: ['./components.component.css']
 })
 export class ComponentsComponent implements OnInit {
-	type: string = null;
-	widget: string = null;
-	colours: string[] = [];
-	selectedValue: string;
 	url: string = "";
-	widgets: Widget[];
-	closeResult: string;
+	userWidgets: Widget[] = [];
+	allWidgets: Widget[] = [new Widget(),]
+
+
 	constructor (private modalService: NgbModal, private authService: AuthService, private data: DataService, private widgetService: WidgetService) {
-		this.colours.push('green', 'red', 'blue');
-		this.selectedValue = this.colours[0];
 	}
 
 	ngOnInit () {
 		this.authService.profile.subscribe(x => this.data.user = new User(x.email, x.name, x.family_name, x.sub, null));
 		this.widgetService.read(this.data.user)
 			.subscribe(x => {
-				this.widgets = x
-				console.log(this.widgets);
+				x.forEach(widget => {
+					this.userWidgets.push(new Widget(widget.id, widget.color, widget.name, widget.userId))
+				});
 			});
 	}
 
+	// apply () {
+	// 	const widget: Widget = new Widget()
+	// 	this.widgetService.create(widget)
+	// 		.subscribe(id => {
+	// 			this.url = "https://localhost:44351/api/widgets/" + id + ".js";
+	// 		});
 
-	// Type of components.
-	chooseComponent (componentType: string) {
-		this.type = componentType;
+	// }
 
+	getWidget (widget: Widget) {
+		console.log(widget);
 	}
-	// The component of type.
-	selectedComponent (widget: string) {
-		this.widget = widget;
-	}
 
-	onSelectedChange (value: string) {
-		this.selectedValue = value;
-
-	}
-	apply () {
-		const widget: Widget = new Widget(null, this.selectedValue, this.widget, this.data.user.Email)
-		this.widgetService.create(widget)
-			.subscribe(id => {
-				this.url = "https://localhost:44351/api/widgets/" + id + ".js";
-			});
-
+	copy (copy: string) {
+		var copyText = document.getElementById(copy) as HTMLInputElement;
+		/* Select the text field */
+		copyText.select();
+		document.execCommand("copy");
 	}
 
 	openVerticallyCentered (content) {
 		console.log("click");
 
 		this.modalService.open(content, { centered: true });
-	  }
+	}
 }
